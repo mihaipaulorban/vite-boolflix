acde2d0e72ed17c08540ebf0875fcac104a74712
mihaipaulorban <mihaipaulorban@gmail.com>
2023-12-15 19:46:57 +0100

    feat: inizio di stile griglia con bootstrap

--- src/components/MainMovies.vue
+++ src/components/MainMovies.vue
@@ -13,38 +13,73 @@ export default {
 </script>
 
 <template>
-  <!-- Info film -->
-  <div v-for="movie in movies" :key="movie.id">
-    <p>{{ movie.original_language }}</p>
-    <p>{{ movie.original_title }}</p>
-    <p>{{ movie.title }}</p>
-    <!-- Valutazione Film -->
-    <div class="rating">
-      <!-- Stelle piene -->
-      <i
-        class="bi bi-star-fill"
-        v-for="star in convertRating(movie.vote_average)"
-        :key="star"
-      ></i>
-      <!-- Stelle vuote -->
-      <i
-        class="bi bi-star"
-        v-for="emptyStar in 5 - convertRating(movie.vote_average)"
-        :key="emptyStar"
-      ></i>
+  <div class="container">
+    <div class="row">
+      <!-- Iterazione sui film -->
+      <div class="col-md-4 col-12" v-for="movie in movies" :key="movie.id">
+        <div class="movie-card">
+          <!-- Immagine del film -->
+          <div class="poster-container">
+            <img
+              :src="
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                  : '/PlaceholderPoster.png'
+              "
+              class="movie-poster"
+            />
+          </div>
+          <!-- Informazioni del film -->
+          <div class="movie-info">
+            <p>{{ movie.original_language }}</p>
+            <p>{{ movie.original_title }}</p>
+            <p>{{ movie.title }}</p>
+            <div class="rating">
+              <!-- Stelle piene -->
+              <i
+                class="bi bi-star-fill"
+                v-for="star in convertRating(movie.vote_average)"
+                :key="star"
+              ></i>
+              <!-- Stelle vuote -->
+              <i
+                class="bi bi-star"
+                v-for="emptyStar in 5 - convertRating(movie.vote_average)"
+                :key="emptyStar"
+              ></i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <img
-      :src="
-        movie.poster_path
-          ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-          : '/PlaceholderPoster.png'
-      "
-    />
   </div>
 </template>
 
 <style scoped lang="scss">
-.rating i {
-  color: gold;
+.container {
+  padding: 20px;
+
+  .movie-card {
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .movie-poster {
+      width: 100%;
+      max-height: 400px;
+      object-fit: cover;
+    }
+
+    .movie-info {
+      width: 100%;
+      text-align: center;
+      margin-top: 10px;
+    }
+
+    .rating i {
+      color: gold;
+    }
+  }
 }
 </style>
